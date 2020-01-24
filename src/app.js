c53941a5627cad8c24fdb3e1c01026afb0474d73
Zi Ye <18612319597@163.com>
2020-01-24 19:28:23 +0800

    [feature] auto open url

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const chalk = require('chalk');
 const path = require('path');
 const route = require('./helper/route');
-
+const openUrl = require('./helper/openUrl');
 const defaultConfig = require('./config/default');
 
 class Server {
@@ -19,6 +19,7 @@ class Server {
     server.listen(this.config.port, this.config.hostname, () => {
       const address = `http://${this.config.hostname}:${this.config.port}`;
       console.log(`Server started at ${chalk.green(address)}`);
+      openUrl(address);
     });
   }
 }
